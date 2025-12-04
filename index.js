c79f9c7b5c8f865172626bc485b08fb4edd757c3
Brzina1921 <brzina1921@gmail.com>
2025-12-04 15:34:36 +0100

    '1.0.0'

---
+++ index.js
@@ -0,0 +1,4 @@
+// index.js (in project root)
+// This file is just to satisfy Vercel's build process
+// Actual routing is handled by vercel.json and the api/ directory
+console.log("Stremio HQ Addon - Build Satisfied");
